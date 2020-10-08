370512d5d25d4e11e83cd0c9580901f74aba4129
kunwar <singh.dhadial@gmail.com>
2020-10-08 06:18:09 +0530

    SubSequence using recursion

--- Foundation Java/Recursion/4.Recursion_On_The_Way_Up/subSequence.java
+++ Foundation Java/Recursion/4.Recursion_On_The_Way_Up/subSequence.java
@@ -1,6 +1,15 @@
 import java.util.*;
 class subSequence{ 
+    public static void subSequence(String ques, String ans){
+        if(ques.length()==0){
+            System.out.println(ans);
+            return;
+        }
+        char ch = ques.charAt(0);
+        subSequence(ques.substring(1),ans+ch);
+        subSequence(ques.substring(1),ans);
+    }
     public static void main(String[] args){
-        System.out.println("hello world");
+        subSequence("abc","");
     }
 }
